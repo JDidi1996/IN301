2e401f39284665635e689d90d78697b46f4f285d
Pailleux Jean-didier <jean-di.pailleux@laposte.net>
2015-09-24 20:37:00 +0200

    "exo4 td1"

---
+++ td1/exo4.c
@@ -0,0 +1,24 @@
+#include "stdio.h"
+
+int main()
+{
+	
+	int n = 4;
+	int i, e, som;	// e = epsilon
+	int S[n];
+	
+	e = n/3;
+	som = 0;
+	i = 1;
+	
+	while ( fabs(S[n+1] - S[n] < e)
+	{
+		S[i] = 1/(i*i);
+		som = som + S[i];
+		
+	}
+	
+	
+	
+	
+}
