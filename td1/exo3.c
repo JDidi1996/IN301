5e7eb6c0b6cd0c5439120327b656076511813eca
Pailleux Jean-didier <jean-di.pailleux@laposte.net>
2015-09-24 20:20:55 +0200

    exo3 td1

--- td1/exo3.c
+++ td1/exo3.c
@@ -2,6 +2,46 @@
 
 int main()
 { 
+
+	int X, Y;
+	int y;
+	
+	X = 23; 
+	Y = 87;
+	y = 0;
+	
+	printf( " %d x %d \n", X, Y);
+	
+	
+	while ( X > 1 ) 
+	{
+		
+		if ( X % 2 == 0) // Pour X pair
+		{
+			
+			Y = 2*Y;
+			X = X/2;
+			printf ( "= %d x %d + %d \n",X, Y, y);
+			
+			
+			
+		}
+	
+		else           // Pour X impair
+		{
+			X = (X-1);
+			printf ( "= %d x %d + %d \n",X, Y, Y);
+			
+			y = y + Y;
+			
+			
+		}
+		
+	}
+	
+	printf ( "= %d \n", y + Y); // y correspond au bloc apres le +, Y au bloc entre le * et le +
+	
+	
 	
 	
 	
