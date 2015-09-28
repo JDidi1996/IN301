427069d71f1f8e90a574dffcd25f61e9fd12cb9b
Pailleux Jean-didier <jean-di.pailleux@laposte.net>
2015-09-28 10:23:53 +0200

    "Factorielle"

--- td1/exo9.c
+++ td1/exo9.c
@@ -6,27 +6,26 @@ int factorielle (int n)
 	
 	while ( n > 0)
 	{
-	if (n <= -1)
+	if ( n <= 1 )
 		f = 1;
 	else
-		f = n * factorielle(--n);
+		f = n * factorielle(n--);
+		
 	}
+	
 	return f;
 }
 
 /* 
- La fonction suivante ne peut marcher car elle tourne en boucle jusqu'à n = 0
- 
+  La fonction donne des résultat faux car elle ne revoie rien.
  
 */
 	
 int main()
 {
 	
-	int n;
-	int fact;
-	n = 6;
+	int a;
+	a = 4;
 	
-	fact = factorielle(n);
-	printf( " le factorielle de %d est %d \n", n, fact);
+	printf( " le factorielle de %d est %d \n", a, factorielle(a));
 }
