8ef0fcd8625bcdb12462f91f5df91484360f6bc2
Pailleux Jean-didier <jean-di.pailleux@laposte.net>
2015-09-28 18:38:47 +0200

    Tableau

--- td1/exo12.c
+++ td1/exo12.c
@@ -53,27 +53,81 @@ void decalage( int *T, int n) // Décale tout les éléments de 1, avec n l'indi
 	}	
 }
 
-void InsererElement( int *T, int elem)
+void InsererElement( int *T, int elem) // On insere un élément d'un tableau trié
 {
-	int i, tmp, MIN;
-	
-	
-	// Triage du tableau
-	
-	for ( i = 0; i < N; i++) 
-	{
-		
-	}	
+	int i;
 	
 	// Décalage du tableau 
 	
+	for ( i = 0; i < N; i++)
+	{
+		if ( elem < *(T+i) ) 
+			{
+				decalage( T, i ); break;
+			}
+	}
 	
 	// Insertion de l'élément
-}
 	
+	*(T+i) = elem;
+	
+	
+}
 
+void Inverse ( int *T ) // Inverse les éléments du tableau
+{
+	int i,tmp;
+	
+	for ( i = 0; i < N/2; i++)
+	{
+		tmp = *(T+i);
+		*(T+i) = *(T+(N-1)-i);
+		*(T+(N-1)-i) = tmp;		
+	}
 
+}
 
+void Double_Neg( int *T) // Supprime les valeurs double ( ou plus) positives en remplaçant par leur valeur négative
+{
+	int i, j;
+	
+	for ( i = 0; i < N; i++)
+	{
+		for ( j = 0; j < N; j++)
+			{
+				if ( (*(T+i) == *(T+j)) && (*(T+i) >= 0) && (i != j) ) *(T+j) = - *(T+j); 
+			}
+	}
+	
+}
+
+int Moyenne_Section(int *T) // Calcul la moyenne des produits de chaque section
+{
+	int m, s; // S = nombre de section
+	int mtmp; //m temporaire
+	int i;
+	s = 1;
+	m = 0;
+	mtmp = 1;
+	
+	for ( i = 0; i < N; i++) // Avec T[0] != 0
+	{
+		if ( (*(T+i) == 0) && (*(T+i-1) != 0 ) ) 
+		{
+			s++;
+			m = m + mtmp;
+			mtmp = 1;
+		}
+		else 
+		{
+			mtmp = mtmp*(*(T+i));
+		}
+	}
+	
+	m = m/s;
+	
+	return m;
+}
 
 int main()
 {
@@ -81,12 +135,24 @@ int main()
 	int T[N];
 	
 	init_case(T);
-	decalage(T, 3);
+	
+	T[4] = 4;
+	T[5] = 0;
+	
 	for ( i = 0; i < N; i++)
 	{
 		printf("Le produit des éléments du tableau est : %d \n", T[i]); 
 	}
-	 
+	
+	InsererElement(T,5); 
+	Double_Neg(T);
+	
+	for ( i = 0; i < N; i++)
+	{
+		printf("%d \n", T[i]); 
+	}
+	
+	printf("La moyenne des sections est %d \n", Moyenne_Section(T));
 	
 	
 	
