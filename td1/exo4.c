b4ef2ea5a4e2c07e8cf29c68302883820649f221
Pailleux Jean-didier <jean-di.pailleux@laposte.net>
2015-10-07 19:37:04 +0200

    Limites

--- td1/exo4.c
+++ td1/exo4.c
@@ -3,20 +3,23 @@
 int main()
 {
 	
-	double i, s;
+	float i, S, Sn;
 	double p; //precision
 
-	s = 1; // Sn
-	p = 0,001;
-		
+	Sn = 1; 
+	S = 0;
+	p = 0.01;
 	i = 1;	
-	while (  (1/(i*i)) >	 p )
+	
+	while (  (Sn-S) > p )
 	{
 		i++ ;
-		s = s + (1/(i*i));
+		S = Sn;
+		Sn = Sn + (1/(i*i));
 		
 	}
 	
-	printf(" La limite de la suite Sn est %f",s);
-	
+	printf(" La limite de la suite Sn est %f",Sn);
+
+return 0;	
 }
